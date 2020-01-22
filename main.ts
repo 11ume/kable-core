ea360fcf0a0971a7125a40bba2279f1232fa87df
11ume <horacioriverods@gmail.com>
2020-01-22 17:46:26 -0300

    add utils

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ import * as EVENTS from './lib/constants/events'
 import * as EVENTS_TYPES from './lib/constants/eventTypes'
 import * as ERRORS from './lib/constants/error'
 import * as Kable from './lib/kable'
+import * as utils from './lib/utils'
 import { pid } from './lib/constants/core'
 
 export = {
@@ -9,5 +10,6 @@ export = {
     , EVENTS_TYPES
     , ERRORS
     , Kable
+    , utils
     , pid
 }
