f142bb3dd85649d98baedba41ef770dfbcda5076
hume <hume@hume@23.org>
2020-01-22 10:14:37 -0300

    imrpovements

--- main.ts
+++ main.ts
@@ -1,29 +1,13 @@
 import * as EVENTS from './lib/constants/events'
 import * as EVENTS_TYPES from './lib/constants/eventTypes'
-import * as Node from './lib/node'
-import * as Store from './lib/store'
-import * as Utils from './lib/utils'
+import * as ERRORS from './lib/constants/error'
 import * as Kable from './lib/kable'
-import * as Orchester from './lib/orchester'
-import * as Transport from './lib/transport/transport'
-import * as Discovery from './lib/discovery'
-import * as Dependency from './lib/dependency'
-import * as NodePicker from './lib/nodePicker'
-import * as Repository from './lib/repository'
-import * as EventDriver from './lib/eventDriver'
+import { pid } from './lib/constants/core'
 
 export = {
     EVENTS
     , EVENTS_TYPES
-    , Node
-    , Store
-    , Utils
+    , ERRORS
     , Kable
-    , Orchester
-    , Transport
-    , Discovery
-    , Dependency
-    , NodePicker
-    , Repository
-    , EventDriver
+    , pid
 }
