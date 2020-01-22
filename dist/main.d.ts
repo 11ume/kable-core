a579428e4c168af815fc1c9c93b5155a3e4737d9
hume <hume@hume@23.org>
2020-01-22 09:37:39 -0300

    Revert "improvements"
    This reverts commit af920c043b2e547fb5b92b217de87f1173308b79.

--- dist/main.d.ts
+++
@@ -1,9 +0,0 @@
-import * as EVENTS from './lib/constants/events';
-import * as EVENTS_TYPES from './lib/constants/eventTypes';
-import * as Kable from './lib/kable';
-declare const _default: {
-    EVENTS: typeof EVENTS;
-    EVENTS_TYPES: typeof EVENTS_TYPES;
-    Kable: typeof Kable;
-};
-export = _default;
