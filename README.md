8548969ab7e93c90303df9c42cedda767a2ef716
11ume <horacioriverods@gmail.com>
2020-01-22 06:28:33 -0300

    first

---
+++ README.md
@@ -0,0 +1,14 @@
+<br>
+<br>
+<br>
+
+<div align="center">
+<img src="https://github.com/11ume/kable/blob/master/images/logo.png" width="200" height="auto"/>
+</div>
+<br>
+<br>
+<br>
+
+**Kable core** — Is a decentralized service discovery and load balancer system for Node.js
+
+**This project is under developement 🔥**
---
+++ main.ts
@@ -0,0 +1,29 @@
+import * as EVENTS from './lib/constants/events'
+import * as EVENTS_TYPES from './lib/constants/eventTypes'
+import * as Node from './lib/node'
+import * as Store from './lib/store'
+import * as Utils from './lib/utils'
+import * as Kable from './lib/kable'
+import * as Orchester from './lib/orchester'
+import * as Transport from './lib/transport/transport'
+import * as Discovery from './lib/discovery'
+import * as Dependency from './lib/dependency'
+import * as NodePicker from './lib/nodePicker'
+import * as Repository from './lib/repository'
+import * as EventDriver from './lib/eventDriver'
+
+export default {
+    EVENTS
+    , EVENTS_TYPES
+    , Node
+    , Store
+    , Utils
+    , Kable
+    , Orchester
+    , Transport
+    , Discovery
+    , Dependency
+    , NodePicker
+    , Repository
+    , EventDriver
+}
