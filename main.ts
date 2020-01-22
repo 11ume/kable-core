11d8467a486cbf43bcddf7046d5c4bc0667a8a25
11ume <horacioriverods@gmail.com>
2020-01-22 17:52:23 -0300

    improvement

--- main.ts
+++ main.ts
@@ -1,15 +1,2 @@
-import * as EVENTS from './lib/constants/events'
-import * as EVENTS_TYPES from './lib/constants/eventTypes'
-import * as ERRORS from './lib/constants/error'
-import * as Kable from './lib/kable'
-import * as utils from './lib/utils'
-import { pid } from './lib/constants/core'
-
-export = {
-    EVENTS
-    , EVENTS_TYPES
-    , ERRORS
-    , Kable
-    , utils
-    , pid
-}
+import Kable from './lib/kable'
+export = Kable
