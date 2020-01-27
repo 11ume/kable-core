7dc6bfb107b9781d823293e8396019cc984bd69b
11ume <horacioriverods@gmail.com>
2020-01-27 02:39:44 -0300

    changes

--- dev/service.ts
+++ dev/service.ts
@@ -1,26 +1,5 @@
-import { NodeEmitter } from './../lib/eventsDriver'
 import kable from '../lib/kable'
 
-const main = async () => {
-    const foo = kable('foo')
-    const bar = kable('bar')
-    await foo.up()
-    await bar.up(false)
-
-    const check = (): Promise<NodeEmitter> => new Promise((resolve) => {
-        foo.suscribe('bar', (payload) => {
-            if (payload.start) {
-                resolve(payload)
-            }
-        })
-
-        bar.start()
-    })
-
-    const n = await check()
-    console.log(n)
-    foo.down()
-    bar.down()
-}
-
-main()
+const foo = kable('foo')
+foo.up()
+foo.suscribe('mongo-service', console.log)
